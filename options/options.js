1997f4f4660880afdd4993f037323365b6279b31
Crowdbotics <engineering@crowdbotics.com>
2023-05-04 06:35:33 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "xsder-7",
+  url: "https://xsder-7.botics.co",
+  api: "https://xsder-7.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
